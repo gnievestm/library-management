9b0f4345ecf58e807f32debc42923d35dcba5e30
Rafael <rafael.escobar@mercadolibre.com>
2023-10-18 11:30:51 -0300

    Add new entry of a Newspaper

---
+++ src/main/java/com/teamcubation/librarymanagement/domain/managers/NewspaperManager.java
@@ -0,0 +1,23 @@
+package com.teamcubation.librarymanagement.domain.managers;
+
+import com.teamcubation.librarymanagement.domain.entities.Newspaper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NewspaperManager {
+
+    private List<Newspaper> catalogue;
+
+    public NewspaperManager() {
+        catalogue = new ArrayList<>();
+    }
+
+    public void addNewspaper(Newspaper newEntry){
+        catalogue.add(newEntry);
+
+    }
+
+
+
+}
---
+++ src/test/java/com/teamcubation/librarymanagement/domain/addNewspaperTest.java
@@ -0,0 +1,23 @@
+package com.teamcubation.librarymanagement.domain;
+
+import com.teamcubation.librarymanagement.domain.entities.Newspaper;
+import com.teamcubation.librarymanagement.domain.managers.NewspaperManager;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+public class addNewspaperTest {
+
+    @Test
+    void addNewEntry(){
+        try {
+            Newspaper entry = new Newspaper(1, "He´s Back!", "2000-07-23");
+            NewspaperManager createEntry = new NewspaperManager();
+            createEntry.addNewspaper(entry);
+        }catch(Exception e){
+
+        }
+
+    }
+}
