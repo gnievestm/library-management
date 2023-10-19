83318aa62a91e26152670725bdc5b9c8ec2d54c5
Rafael <rafael.escobar@mercadolibre.com>
2023-10-19 09:23:21 -0300

    Update for the excepton who search for a newspaper

---
+++ src/main/java/com/teamcubation/librarymanagement/domain/exceptions/Newspaper/NewspaperNotFound.java
@@ -0,0 +1,8 @@
+package com.teamcubation.librarymanagement.domain.exceptions.Newspaper;
+
+public class NewspaperNotFound extends Exception {
+
+    public NewspaperNotFound(){
+        super("There is not a newspaper with this data");
+    }
+}
--- src/test/java/com/teamcubation/librarymanagement/domain/NewspaperServicesTest.java
+++ src/test/java/com/teamcubation/librarymanagement/domain/NewspaperServicesTest.java
@@ -1,6 +1,8 @@
 package com.teamcubation.librarymanagement.domain;
 
 import com.teamcubation.librarymanagement.domain.entities.Newspaper;
+import com.teamcubation.librarymanagement.domain.exceptions.Newspaper.MissingDateOrHeadline;
+import com.teamcubation.librarymanagement.domain.exceptions.Newspaper.NewspaperNotFound;
 import com.teamcubation.librarymanagement.domain.managers.NewspaperManager;
 import org.junit.jupiter.api.Test;
 
@@ -10,17 +12,25 @@ import java.util.List;
 public class NewspaperServicesTest {
 
     @Test
-    void addNewEntry(){
+    void addNewEntry() throws MissingDateOrHeadline {
 
+        try {
             Newspaper entry = new Newspaper(1, "He´s Back!", "2000-07-23");
-            NewspaperManager manageEntry = new NewspaperManager();
-            manageEntry.addNewspaper(entry);
+            if (entry.getDate().isEmpty() || entry.getHeadline().isEmpty()) {
+                throw new MissingDateOrHeadline();
+            } else {
+                NewspaperManager manageEntry = new NewspaperManager();
+                manageEntry.addNewspaper(entry);
+            }
+        } catch (MissingDateOrHeadline e) {
+
+        }
 
 
     }
 
     @Test
-    void viewNewspaper(){
+    void viewNewspaper() {
 
         Newspaper entry = new Newspaper(1, "He´s Back!", "2000-07-23");
         NewspaperManager manageEntry = new NewspaperManager();
@@ -29,14 +39,25 @@ public class NewspaperServicesTest {
         manageEntry.viewNewspaper();
 
     }
+
     @Test
-    void viewNewspaperRequest(){
+    void viewNewspaperRequest() throws NewspaperNotFound {
 
-        Newspaper entry = new Newspaper(1, "He´s Back!", "2000-07-23");
-        NewspaperManager manageEntry = new NewspaperManager();
-        manageEntry.addNewspaper(entry);
+        try {
+            Newspaper entry = new Newspaper(1, "The last stand", "2008-02-03");
+            if (entry.getDate().isEmpty() || entry.getHeadline().isEmpty()) {
+                throw new NewspaperNotFound();
+            } else {
+                NewspaperManager manageEntry = new NewspaperManager();
+                manageEntry.addNewspaper(entry);
 
-        manageEntry.viewNewspaperRequest(entry);
+                manageEntry.viewNewspaperRequest(entry);
+            }
+
+        } catch (NewspaperNotFound e) {
+
+        }
+
 
     }
 }
