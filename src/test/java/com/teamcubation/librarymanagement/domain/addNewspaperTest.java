aa688748c0a323c8e0db941523b7cee3299f79f6
Rafael <rafael.escobar@mercadolibre.com>
2023-10-20 09:34:55 -0300

    Update on unit test for addNewspaper

--- src/test/java/com/teamcubation/librarymanagement/domain/addNewspaperTest.java
+++ src/test/java/com/teamcubation/librarymanagement/domain/addNewspaperTest.java
@@ -1,31 +1,31 @@
 package com.teamcubation.librarymanagement.domain;
 
 import com.teamcubation.librarymanagement.domain.entities.Newspaper;
+import com.teamcubation.librarymanagement.domain.exceptions.Newspaper.MissingDateOrHeadline;
 import com.teamcubation.librarymanagement.domain.managers.NewspaperManager;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 import java.util.Date;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class addNewspaperTest {
 
-    
+
     @Test
     void addNewEntry() throws MissingDateOrHeadline {
 
-        try {
-            Newspaper entry = new Newspaper(1, "He´s Back!", "2000-07-23");
-            if (entry.getDate().isEmpty() || entry.getHeadline().isEmpty()) {
-                throw new MissingDateOrHeadline();
-            } else {
-                NewspaperManager manageEntry = new NewspaperManager();
-                manageEntry.addNewspaper(entry);
-            }
-        } catch (MissingDateOrHeadline e) {
+        Newspaper entry = new Newspaper(1, "He´s Back!", "2000-07-23");
+        NewspaperManager manageEntry = new NewspaperManager();
+        assertTrue(manageEntry.addNewspaper(entry), "The upload of the entry was succeeded");
+    }
+    @Test
+    void addNewEntryWithOutHeadlineOrDate() throws MissingDateOrHeadline {
 
-        }
-
-
+        Newspaper entry = new Newspaper(1, "", "2000-07-23");
+        NewspaperManager manageEntry = new NewspaperManager();
+        assertTrue(manageEntry.addNewspaper(entry), "the upload the entry is negated");
     }
 
     
