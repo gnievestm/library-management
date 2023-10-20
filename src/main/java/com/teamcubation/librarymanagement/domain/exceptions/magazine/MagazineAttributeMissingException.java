50b1c3a07bd883a8ce6fab33ba91d0d52ffd8273
gnievestm <144127268+gnievestm@users.noreply.github.com>
2023-10-20 10:29:49 -0300

    Revert "Feature/tm23-15 (#5)" (#16)
    This reverts commit 3459b0bdc481d9313adb0b1709fbb3e2477d50c8.

--- src/main/java/com/teamcubation/librarymanagement/domain/exceptions/magazine/MagazineAttributeMissingException.java
+++
@@ -1,7 +0,0 @@
-package com.teamcubation.librarymanagement.domain.exceptions.magazine;
-
-public class MagazineAttributeMissingException extends Exception {
-    public MagazineAttributeMissingException() {
-        super("Your magazine attributes are void");
-    }
-}
