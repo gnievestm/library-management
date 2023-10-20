31d15d83c4212ce0d4923eaef6d7a093c278d77f
Rafael <rafael.escobar@mercadolibre.com>
2023-10-20 09:37:15 -0300

    Update on NewspaperManager

--- src/main/java/com/teamcubation/librarymanagement/domain/managers/NewspaperManager.java
+++ src/main/java/com/teamcubation/librarymanagement/domain/managers/NewspaperManager.java
@@ -1,6 +1,7 @@
 package com.teamcubation.librarymanagement.domain.managers;
 
 import com.teamcubation.librarymanagement.domain.entities.Newspaper;
+import com.teamcubation.librarymanagement.domain.exceptions.Newspaper.MissingDateOrHeadline;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,13 +10,17 @@ public class NewspaperManager {
 
     private List<Newspaper> catalogue;
 
-    public NewspaperManager() {
-        catalogue = new ArrayList<>();
-    }
+    public boolean addNewspaper(Newspaper newEntry) throws MissingDateOrHeadline {
+        try {
+            if (newEntry.getDate().isEmpty() || newEntry.getHeadline().equals("")) {
+                throw new MissingDateOrHeadline();
+            } else {
+                catalogue.add(newEntry);
+            }
+        } catch (MissingDateOrHeadline e) {
 
-    public void addNewspaper(Newspaper newEntry){
-        catalogue.add(newEntry);
-
+        }
+        return true;
     }
 
 
