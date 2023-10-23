bd14d853f3805552bcb853f0467558aef315b71c
gnievestm <gaston.nieves@teamcubation.com>
2023-10-23 12:08:19 -0300

    Revert "Solving conflicts"
    This reverts commit 66d695ea91a83b28014cbcb1ca0f976277837a49, reversing
    changes made to f90aaf9f7da972ce0e69f6743583fed7ff419a56.

--- src/test/java/com/teamcubation/librarymanagement/domain/RoomServiceTest.java
+++ src/test/java/com/teamcubation/librarymanagement/domain/RoomServiceTest.java
@@ -4,11 +4,6 @@ import com.teamcubation.librarymanagement.domain.entities.Room;
 import com.teamcubation.librarymanagement.domain.exceptions.room.*;
 import com.teamcubation.librarymanagement.domain.managers.RoomManager;
 import com.teamcubation.librarymanagement.application.service.RoomService;
-import com.teamcubation.librarymanagement.domain.exceptions.room.RoomAlreadyExistsException;
-import com.teamcubation.librarymanagement.domain.exceptions.room.RoomIncompleteFieldsException;
-import com.teamcubation.librarymanagement.domain.exceptions.room.RoomInvalidIdException;
-import com.teamcubation.librarymanagement.domain.exceptions.room.RoomNotFoundException;
-
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -36,11 +31,11 @@ public class RoomServiceTest {
 
         assertThrows(RoomAlreadyExistsException.class, () -> {
             RoomService roomService = RoomService.getInstance();
-
+            Room room = new Room(2, "name", "address");
             roomService.addRoom(2,"name", "address");
             roomService.addRoom(2,"name", "address");
-
         });
+
     }
 
     @Test
@@ -49,6 +44,7 @@ public class RoomServiceTest {
         assertThrows(RoomInvalidIdException.class, () -> {
             Room room = new Room(-1, "name", "address");
         });
+
     }
 
     @Test
@@ -57,6 +53,7 @@ public class RoomServiceTest {
        assertThrows(RoomIncompleteFieldsException.class, () -> {
             Room room = new Room(null, "address");
        });
+
     }
 
     @Test
@@ -74,7 +71,7 @@ public class RoomServiceTest {
 
         roomManager.reserveRoom(room1);
 
-        List<Room> testReservedRooms = new ArrayList<>();
+        List <Room> testReservedRooms = new ArrayList<>();
         testReservedRooms.add(room1);
 
         assertEquals(testAvailableRooms, roomManager.getAvailableRooms());
@@ -91,24 +88,4 @@ public class RoomServiceTest {
 
     }
 
-    @Test
-    void reserveRoom() throws RoomInvalidIdException, RoomIncompleteFieldsException, RoomAlreadyExistsException, RoomNotFoundException, RoomAlreadyReservedException {
-        RoomManager roomManager = new RoomManager();
-        Room room1 = new Room(1, "name", "address");
-
-        roomManager.addRoom(room1);
-
-        assertTrue(roomManager.reserveRoom(room1));
-
-    }
-
-    @Test
-    void reserveRoomWithoutAvailableRooms() {
-
-        assertThrows(RoomNotFoundException.class, () -> {
-            RoomManager roomManager = new RoomManager();
-            Room room1 = new Room(1, "name", "address");
-            roomManager.reserveRoom(room1);
-        });
-    }
 }
