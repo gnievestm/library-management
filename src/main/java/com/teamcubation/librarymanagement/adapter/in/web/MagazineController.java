0b9052620f105893a687decf685108e219b43095
leosilva12 <leonardo.silva@mercadolibre.com>
2023-10-26 12:46:20 -0300

    implemented date

--- src/main/java/com/teamcubation/librarymanagement/adapter/in/web/MagazineController.java
+++ src/main/java/com/teamcubation/librarymanagement/adapter/in/web/MagazineController.java
@@ -6,6 +6,7 @@ import com.teamcubation.librarymanagement.domain.entities.Magazine;
 import com.teamcubation.librarymanagement.domain.exceptions.magazine.MagazineAttributeMissingException;
 import com.teamcubation.librarymanagement.domain.exceptions.magazine.MagazineNotAvailableException;
 import com.teamcubation.librarymanagement.domain.exceptions.magazine.MagazineNotExistException;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -32,21 +33,21 @@ public class MagazineController {
         return ResponseEntity.ok(magazinePort.searchMagazine(magazineName));
     }
 
-    @PostMapping(path = "/api/magazines")
-    public ResponseEntity<Magazine> createUser(@RequestBody Magazine magazine) throws MagazineAttributeMissingException {
+    @PostMapping(path = "/api/magazines/create")
+    public ResponseEntity<Magazine> addMagazine(@RequestParam("dateOfJoining")@DateTimeFormat(pattern="dd-MM-yyyy")@RequestBody Magazine magazine ) throws MagazineAttributeMissingException {
         magazinePort.addMagazine(magazine);
         return ResponseEntity.ok(magazine);
 
     }
 
-    @PostMapping(path = "/api/magazines")
+    @PostMapping(path = "/api/magazines/borrow")
     public ResponseEntity<Magazine> borrowMagazine(@RequestBody Magazine magazine) throws MagazineNotAvailableException {
 
         magazinePort.borrowMagazine(magazine);
         return ResponseEntity.ok(magazine);
     }
 
-    @PostMapping(path = "/api/magazines")
+    @PostMapping(path = "/api/magazines/return")
     public ResponseEntity<Magazine> returnMagazine(@RequestBody Magazine magazine) {
         magazinePort.returnMagazine(magazine);
         return ResponseEntity.ok(magazine);
