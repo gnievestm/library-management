c333bb7044202a23aea1d7beed16f107c1bcc827
leosilva12 <leonardo.silva@mercadolibre.com>
2023-10-26 11:28:53 -0300

    implemented HTTP 400 error

--- src/main/java/com/teamcubation/librarymanagement/adapter/in/web/MagazineController.java
+++ src/main/java/com/teamcubation/librarymanagement/adapter/in/web/MagazineController.java
@@ -6,12 +6,12 @@ import com.teamcubation.librarymanagement.domain.entities.Magazine;
 import com.teamcubation.librarymanagement.domain.exceptions.magazine.MagazineAttributeMissingException;
 import com.teamcubation.librarymanagement.domain.exceptions.magazine.MagazineNotAvailableException;
 import com.teamcubation.librarymanagement.domain.exceptions.magazine.MagazineNotExistException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.*;
 
-
 import java.util.List;
+import java.util.Objects;
 
 @RestController
 public class MagazineController {
@@ -35,17 +35,27 @@ public class MagazineController {
 
     @PostMapping(path = "/api/magazines")
     public ResponseEntity<Magazine> createUser(@RequestBody Magazine magazine) throws MagazineAttributeMissingException {
+        if (magazine.getName() == null || magazine.getDate() == null || Objects.equals(magazine.getName(), "")) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
         magazinePort.addMagazine(magazine);
         return ResponseEntity.ok(magazine);
     }
 
     @PostMapping(path = "/api/magazines")
     public ResponseEntity<Magazine> borrowMagazine(@RequestBody Magazine magazine) throws MagazineNotAvailableException {
+        if (magazine.getName() == null || magazine.getDate() == null || Objects.equals(magazine.getName(), "")) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
         magazinePort.borrowMagazine(magazine);
         return ResponseEntity.ok(magazine);
     }
+
     @PostMapping(path = "/api/magazines")
-    public ResponseEntity<Magazine> returnMagazine(@RequestBody Magazine magazine){
+    public ResponseEntity<Magazine> returnMagazine(@RequestBody Magazine magazine) {
+        if (magazine.getName() == null || magazine.getDate() == null || Objects.equals(magazine.getName(), "")){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
         magazinePort.returnMagazine(magazine);
         return ResponseEntity.ok(magazine);
     }
